b9e1ab7160cbdab71de2e0d1a8596b05d5bf18b0
Chris Nolin <cnolin@ladmins-MacBook-Pro-2.local>
2020-05-21 19:54:38 -0600

    initial commit for new-nlp project

---
+++ src/client/index.js
@@ -0,0 +1,16 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/form.css'
+import './styles/footer.css'
+import './styles/header.css'
+
+console.log(Client.checkForName);
+
+alert("I EXIST")
+
+export {
+    checkForName,
+    handleSubmit
+}
